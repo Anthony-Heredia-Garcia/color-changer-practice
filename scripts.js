55ea54d0a8038e842b792cbccecad6d8f9af7b3b
Anthony-Heredia-Garcia <99502244+Anthony-Heredia-Garcia@users.noreply.github.com>
2022-06-01 17:00:24 -0400

    switch case function added

--- scripts.js
+++ scripts.js
@@ -2,23 +2,40 @@ const redBtn = document.getElementById("red");
 const blueBtn = document.getElementById("blue");
 const greenBtn = document.getElementById("green");
 const child = document.querySelector(".child");
-let childBG = child.style.background;
 
-redBtn.addEventListener("click", redBG);
-blueBtn.addEventListener("click", blueBG);
-greenBtn.addEventListener("click", greenBG);
+redBtn.addEventListener("click", changeColor);
+blueBtn.addEventListener("click", changeColor);
+greenBtn.addEventListener("click", changeColor);
 
-function redBG() {
-  childBG = "red";
-  redBtn.style.backgroundColor = "red";
-}
-function blueBG() {
-  childBG = "blue";
-  blueBtn.style.backgroundColor = "blue";
-}
-function greenBG() {
-  childBG = "green";
-  greenBtn.style.backgroundColor = "green";
-}
+// function redBG() {
+//   childBG = "red";
+//   redBtn.style.backgroundColor = "red";
+// }
+// function blueBG() {
+//   childBG = "blue";
+//   blueBtn.style.backgroundColor = "blue";
+// }
+// function greenBG() {
+//   childBG = "green";
+//   greenBtn.style.backgroundColor = "green";
+// }
 
-console.log(child.style.background);
+function changeColor(event) {
+  switch (event.target) {
+    default:
+      child.style.background = "white";
+      break;
+
+    case redBtn:
+      child.style.background = "red";
+      break;
+
+    case blueBtn:
+      child.style.background = "blue";
+      break;
+
+    case greenBtn:
+      child.style.background = "green";
+      break;
+  }
+}
